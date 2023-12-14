f356693befd6cb0225d3658552182a1f52eea667
Juan David Arenas <jdarenas676@misena.edu.co>
2023-12-14 17:15:56 -0500

    modifcacion de componentes, creacion y utilizacion useReduce, add localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { products as initialProducts } from './mocks/products.json'
 import { Header } from "./components/Header.jsx"
 import { Footer } from "./components/Footer.jsx"
 import { useFilters } from "./hooks/useFilters.js"
+import { Cart } from "./components/Cart.jsx"
+import { CartProvider } from "./context/cart.jsx"
 
 
 
@@ -11,11 +13,12 @@ function App() {
   const filteredProducts = filterProducts(initialProducts)
 
   return (
-    <>
+    <CartProvider >
       <Header />
+      <Cart />
       <Products products={filteredProducts}/>
       <Footer />
-    </>
+    </CartProvider>
   )
 }
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,24 @@
+/* eslint-disable react/prop-types */
 import './Products.css'
-import { AddToCartIcon } from './Icons.jsx'
+import { AddToCartIcon, RemoveFromCartIcon } from './Icons.jsx'
+import { useCart } from '../hooks/useCart.js'
 
 // eslint-disable-next-line react/prop-types
 export function Products ({ products }) {
+
+    const { addToCart, removeFromCart, cart } = useCart()
+
+    const checkProductInCart = product => {
+        return cart.some(item => item.id === product.id)
+    }
+
     return (
         <main className='products'>
             <ul>
-                {
-                    // eslint-disable-next-line react/prop-types
-                    products.slice(0,10).map(product =>(
+                {products.slice(0,10).map(product =>{
+                    const isProductInCart = checkProductInCart(product)
+
+                    return (
                         <li key={product.id}>
                             <img 
                                 src={product.thumbnail}
@@ -18,13 +28,25 @@ export function Products ({ products }) {
                                 <strong>{product.title}</strong> - ${product.price}
                             </div>
                             <div>
-                                <button>
-                                    <AddToCartIcon />
+                                <button 
+                                    style={{
+                                        backgroundColor: isProductInCart ? 'red' : '#90f'
+                                    }}
+                                    onClick={() => {
+                                        isProductInCart
+                                            ? removeFromCart(product)
+                                            : addToCart(product)
+                                    }}>
+                                    {
+                                        isProductInCart
+                                            ? <RemoveFromCartIcon />
+                                            : <AddToCartIcon />
+                                    }
                                 </button>
                             </div>
                         </li>
-                    ))
-                }
+                    )
+                })}
             </ul>
         </main>
     )
